7905f652df44fb0b62c57987209041b690513623
Fayaaz Ahmed <fayaaz@users.noreply.github.com>
2023-12-24 02:32:31 +0000

    Sway config (#70)
    * Add i3blocks scripts
    
    * sway config fixes
    
    * Remove packages and mask services
    
    * Add wallpaper
    
    * Use newer github runner
    
    * manually mask pipewire services
    
    ---------
    
    Co-authored-by: Fayaaz Ahmed <fayaaz.ahmed@censornet.com>

--- stage3/02-desktop/files/i3blocks/scripts/shutdown.sh
+++ stage3/02-desktop/files/i3blocks/scripts/shutdown.sh
@@ -1,1 +1,1 @@
-i3-nagbar -t error -m 'You pressed the power button. Do you really want to shutdown? This will turn off the device.' -b 'Yes, shudown!' 'sudo shutdown -h now'
+swaynag -t error -m 'You pressed the power button. Do you really want to shutdown? This will turn off the device.' -B 'Yes, shutdown!' 'sudo shutdown -h now'
--- stage3/04-enable-wayland/01-run.sh
+++ stage3/04-enable-wayland/01-run.sh
@@ -1,12 +1,28 @@
+# Enable wayland
 on_chroot << EOF
 	SUDO_USER=pi raspi-config nonint do_boot_behaviour B4
 	raspi-config nonint do_xcompmgr 0
 	SUDO_USER=pi raspi-config nonint do_wayland W2
 EOF
 
+# Remove cups
 on_chroot << EOF
-    systemctl --user -M pi@ mask pipewire
-    systemctl --user -M pi@ mask pipewire-pulse
-    apt-get purge -y cups cups-common
+    apt-get purge -y cups cups-common system-config-printer printer-driver-* pocketsphinx-* pi-printer-support
     apt-get autoremove -y
+EOF
+
+# Mask pipewire services
+on_chroot << EOF
+    systemctl mask pipewire
+    systemctl mask pipewire-pulse
+    systemctl mask wireplumber
+    systemctl mask --global pipewire
+    systemctl mask --global pipewire-pulse
+    systemctl mask --global wireplumber
+    mkdir -p /home/pi/.config/systemd/user/
+    ln -sf /dev/null /home/pi/.config/systemd/user/pipewire.service
+    ln -sf /dev/null /home/pi/.config/systemd/user/pipewire.socket
+    ln -sf /dev/null /home/pi/.config/systemd/user/pipewire-pulse
+    ln -sf /dev/null /home/pi/.config/systemd/user/pipewire-pulse.socket
+    ln -sf /dev/null /home/pi/.config/systemd/user/wireplumber
 EOF
